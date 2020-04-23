aed7a48f4416d380c4e0ff54429ff6c06be258d8
Joann_Brady <jbrady11934@optimum.net>
2020-04-22 21:43:39 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
-end
+ def my_each(words)      # put argument(s) here
+ i = 0
+ while i < words.length  
+   yield (words[i]) # code here
+ i=i+1
+ end
+ words
+ end
+
+
