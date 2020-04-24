3237ce4a3e7c5c5f91f354b8069035dea4ebd06c
Anmol Kaur <kaur.anmol26@gmail.com>
2020-04-24 23:23:58 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,34 +1,8 @@
-# def my_each(array)
-#   i = 0
-#   while i < array.length
-#     yield(array[i])
-#     i += 1
-#   end
-#   array
-# end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def my_each(words)
-  
-  count = 0
-  while count < words.length
-   yeild(array[count])
-   count += 1
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
- word
+  array
 end
-
-  
