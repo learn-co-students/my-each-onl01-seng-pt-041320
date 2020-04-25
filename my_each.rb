fc1d71798e83af53278fef6232f3c0c6f1ee4300
Harrison Price <hprice518@gmail.com>
2020-04-25 20:54:21 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(array)
+  if block_given?
+  num = 0
+    while num < array.length
+      yield (array[num])
+      num += 1 
+    end
+    my_each(array)
+  else 
+    "No block given!"
+  end
+  
 end
+
