48439dfe156135f79bf6128a5899ab8b5e07a42c
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-25 14:24:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,34 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  # if block_given?
+  i = 0
+  
+ while i < collection.length
+ yield(collection[i])
+ i = i + 1
+ end
+
+collection
 end
+# else
+#   puts "no collection given"
+#   end
+
+
+
+
+
+
+# def my_each(name)
+# if name do |name|
+#   puts "name"
+# end
+
+
+
+
+
+# def my_each(name)
+#   my_each (name) do |name|
+# end
+#   puts name
+# end
